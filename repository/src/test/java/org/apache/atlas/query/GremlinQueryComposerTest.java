c7b10754ec33fb80df5e70f569e8cc0d032d4204
Verdan Mahmood <verdan.mahmood@gmail.com>
2019-08-20 21:20:59 +0200

    Fixes test cases

--- repository/src/test/java/org/apache/atlas/query/GremlinQueryComposerTest.java
+++ repository/src/test/java/org/apache/atlas/query/GremlinQueryComposerTest.java
@@ -162,7 +162,7 @@ public class GremlinQueryComposerTest {
         verify("from DB where name = \"Reporting\" select name, owner", getExpected(exSel, exMain));
         verify("from DB where (name = \"Reporting\") select name, owner", getExpected(exSel, exMain));
         verify("Table where Asset.name like \"Tab*\"",
-                "g.V().has('__typeName', 'Table').has('Asset.__s_name', org.janusgraph.core.attribute.Text.textRegex(\"Tab.*\")).dedup().limit(25).toList()");
+                "g.V().has('__typeName', 'Table').has('Asset.__s_name', org.janusgraph.core.attribute.Text.textContains(\"Tab.*\")).dedup().limit(25).toList()");
         verify("from Table where (db.name = \"Reporting\")",
                 "g.V().has('__typeName', 'Table').out('__Table.db').has('DB.name', eq(\"Reporting\")).dedup().in('__Table.db').dedup().limit(25).toList()");
     }
